d8b9282aa6ceadcdb17cc414affbbf818e02a8c7
railsbeginner <bartosz.dyja@gmail.com>
2016-02-10 07:53:46 +0100

    Add rspec gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ group :development, :test do
   gem 'byebug'
 end
 
+group :test do
+  gem 'rspec-rails', '~> 3.0'
+end
+
 group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, contstraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1 do
+      resources :users
     end
   end
 end
